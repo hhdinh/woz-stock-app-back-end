aa42ea158283dd67d26d18e70ee40d6a8d5e1a06
Hoa Dinh <hoadinh.rdms@gmail.com>
2020-06-14 18:04:06 -0500

    first commit

---
+++ secret.js
@@ -0,0 +1,3 @@
+const API_TOKEN = 'Tpk_ca343db84f4b45d8b7d7c50923687dee';
+
+module.exports = API_TOKEN;
---
+++ server/db/models/portfolio.js
@@ -0,0 +1,42 @@
+const Sequelize = require("sequelize");
+const db = require("../db");
+const Stock = require("./Stock");
+
+const Portfolio = db.define("portfolio", {
+  quantity: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    defaultValue: 1,
+    validate: {
+      min: 1,
+    },
+  },
+});
+
+Portfolio.getPortfolioStocks = async (portfolio) => {
+  console.log("Portfolio.getPortfolioStocks => portfolio", portfolio.length);
+  const stocks = [];
+  let symbols = "";
+
+  for (let i = 0; i < portfolio.length; i++) {
+    const item = portfolio[i];
+    const stock = await Stock.findByPk(item.stockId);
+
+    symbols += `${stock.symbol},`;
+
+    stocks.push({
+      stockId: item.stockId,
+      symbols: stock.symbol,
+      name: stock.name,
+      quantity: item.quantity,
+    });
+  }
+  // remove white space at the end
+  symbols = symbols.slice(0, -1);
+
+  const portfolioStocks = { symbols, stocks };
+
+  return portfolioStocks;
+};
+
+module.exports = Portfolio;
